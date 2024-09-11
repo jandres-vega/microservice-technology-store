9556153bb65b42a29af2b8658703f8b4bba94a8a
jandres-vega <jandresvega@itsa.edu.co>
2024-09-11 18:28:00 -0500

    Feact: Add entity product

---
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/entity/Product.java
@@ -0,0 +1,19 @@
+package com.store.technology.productcatalogservice.persistence.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id_product")
+    private String idProduct;
+    @Column(name = "name_product")
+    private String nameProduct;
+    private String description;
+    private Double price;
+    private Integer stock;
+    private String idCategory;
+
+}
