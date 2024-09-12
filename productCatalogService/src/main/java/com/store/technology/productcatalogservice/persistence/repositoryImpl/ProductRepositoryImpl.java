e5028d14970e1a77f38cc9da94b4e51438222e74
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 17:12:23 -0500

    feat: Update ProductRepositoryImpl to use request and response DTOs

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/repositoryImpl/ProductRepositoryImpl.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/repositoryImpl/ProductRepositoryImpl.java
@@ -1,7 +1,12 @@
 package com.store.technology.productcatalogservice.persistence.repositoryImpl;
 
+import com.store.technology.productcatalogservice.domain.dto.request.ProductRequestDTO;
+import com.store.technology.productcatalogservice.domain.dto.response.ProductResponseDTO;
 import com.store.technology.productcatalogservice.domain.repository.ProductRepository;
+import com.store.technology.productcatalogservice.persistence.crud.ProductCrudRepository;
 import com.store.technology.productcatalogservice.persistence.entity.Product;
+import com.store.technology.productcatalogservice.persistence.mapper.ProductMapper;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,14 +14,24 @@ import java.util.List;
 @Repository
 public class ProductRepositoryImpl implements ProductRepository {
 
+    private final ProductCrudRepository productCrudRepository;
+    private final ProductMapper productMapper;
+
+    public ProductRepositoryImpl(ProductCrudRepository productCrudRepository, ProductMapper productMapper) {
+        this.productCrudRepository = productCrudRepository;
+        this.productMapper = productMapper;
+    }
+
 
     @Override
-    public Product save(Product product) {
-        return null;
+    public ProductResponseDTO save(ProductRequestDTO product) {
+        Product productSaved = productMapper.toProduct(product);
+        return productMapper.toProductResponseDTO(productCrudRepository.save(productSaved));
     }
 
     @Override
-    public List<Product> findAll() {
-        return List.of();
+    public List<ProductResponseDTO> findAll() {
+        List<Product> products = (List<Product>) productCrudRepository.findAll();
+        return productMapper.toProductResponseDTO(products);
     }
 }
