18fa446b4cc42ca820f273b2017e6034bd6c739a
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 12:46:04 -0500

    feat: Update CategoryRepository to use request and response DTOs

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/repository/CategoryRepository.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/repository/CategoryRepository.java
@@ -1,11 +1,13 @@
 package com.store.technology.productcatalogservice.domain.repository;
 
-import com.store.technology.productcatalogservice.domain.dto.CategoryDTO;
+import com.store.technology.productcatalogservice.domain.dto.request.CategoryRequestDTO;
+import com.store.technology.productcatalogservice.domain.dto.response.CategoryResponseDTO;
 
 import java.util.List;
 
 public interface CategoryRepository {
 
-    CategoryDTO save(CategoryDTO categoryDTO);
-    List<CategoryDTO> findAll();
+    CategoryRequestDTO save(CategoryRequestDTO categoryDTO);
+    List<CategoryResponseDTO> findAll();
+    String deleteCategoryById(String id);
 }
