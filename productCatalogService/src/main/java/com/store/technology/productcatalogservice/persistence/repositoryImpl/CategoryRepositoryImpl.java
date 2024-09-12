6afa1993582264f58a2804c79581baf5cf7d3ff3
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 10:50:30 -0500

    feat: Update CategoryRepositoryImpl to use CategoryDTO for saving and retrieving categories

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/repositoryImpl/CategoryRepositoryImpl.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/repositoryImpl/CategoryRepositoryImpl.java
@@ -1,8 +1,10 @@
 package com.store.technology.productcatalogservice.persistence.repositoryImpl;
 
+import com.store.technology.productcatalogservice.domain.dto.CategoryDTO;
 import com.store.technology.productcatalogservice.domain.repository.CategoryRepository;
 import com.store.technology.productcatalogservice.persistence.crud.CategoryCrudRepository;
 import com.store.technology.productcatalogservice.persistence.entity.Category;
+import com.store.technology.productcatalogservice.persistence.mapper.CategoryMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,17 +14,22 @@ public class CategoryRepositoryImpl implements CategoryRepository {
 
     private final CategoryCrudRepository categoryCrudRepository;
 
-    public CategoryRepositoryImpl(CategoryCrudRepository categoryCrudRepository) {
+    private final CategoryMapper categoryMapper;
+
+    public CategoryRepositoryImpl(CategoryCrudRepository categoryCrudRepository, CategoryMapper categoryMapper) {
         this.categoryCrudRepository = categoryCrudRepository;
+        this.categoryMapper = categoryMapper;
     }
 
     @Override
-    public Category save(Category category) {
-        return null;
+    public CategoryDTO save(CategoryDTO categoryDTO) {
+        Category category = categoryMapper.toCategory(categoryDTO);
+        return categoryMapper.toCategoryDTO(categoryCrudRepository.save(category));
     }
 
     @Override
-    public List<Category> findAll() {
-        return List.of();
+    public List<CategoryDTO> findAll() {
+        List<Category> categories = (List<Category>) categoryCrudRepository.findAll();
+        return categories.stream().map(categoryMapper::toCategoryDTO).toList();
     }
 }
