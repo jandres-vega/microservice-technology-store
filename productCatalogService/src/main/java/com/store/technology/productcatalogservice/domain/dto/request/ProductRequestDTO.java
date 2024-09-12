47652264029e79e9b3b00537fac6f984b39e331a
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 17:12:07 -0500

    feat: Update ProductRequestDTO to use snake_case for name_product and id_category fields

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/dto/request/ProductRequestDTO.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/dto/request/ProductRequestDTO.java
@@ -1,19 +1,13 @@
 package com.store.technology.productcatalogservice.domain.dto.request;
 
-import com.store.technology.productcatalogservice.persistence.entity.Category;
-import com.store.technology.productcatalogservice.persistence.entity.Image;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class ProductRequestDTO {
 
-    private String idProduct;
-    private String nameProduct;
+    private String name_product;
     private String description;
     private Double price;
     private Integer stock;
-    private Category category;
-    private List<Image> images;
+    private String id_category;
 }
