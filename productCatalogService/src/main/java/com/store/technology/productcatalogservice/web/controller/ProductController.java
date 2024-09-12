cca324a7641e8b640cd1c346304ea1b2f36edf38
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 17:12:29 -0500

    feat: Update ProductController to include saveProduct endpoint

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/web/controller/ProductController.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/web/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.store.technology.productcatalogservice.web.controller;
 
+import com.store.technology.productcatalogservice.domain.dto.request.ProductRequestDTO;
+import com.store.technology.productcatalogservice.domain.dto.response.ProductResponseDTO;
 import com.store.technology.productcatalogservice.domain.service.ProductService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("products")
@@ -17,7 +20,13 @@ public class ProductController {
     }
 
     @GetMapping("/all")
-    public ResponseEntity<?> getProducts(){
-        return ResponseEntity.ok(productService.getProducts());
+    public ResponseEntity<List<ProductResponseDTO>> getProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProducts());
     }
+
+    @PostMapping("/save")
+    public ResponseEntity<ProductResponseDTO> saveProduct(@RequestBody ProductRequestDTO product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.saveProduct(product));
+    }
+
 }
