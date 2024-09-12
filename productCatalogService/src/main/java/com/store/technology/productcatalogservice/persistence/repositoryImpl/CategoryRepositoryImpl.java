c2d6e6db06645fec2c2b4d0bb36d2d9b091659d9
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 10:22:30 -0500

    feat: Add CategoryCrudRepository to CategoryRepositoryImpl constructor

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/repositoryImpl/CategoryRepositoryImpl.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/repositoryImpl/CategoryRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.store.technology.productcatalogservice.persistence.repositoryImpl;
 
 import com.store.technology.productcatalogservice.domain.repository.CategoryRepository;
+import com.store.technology.productcatalogservice.persistence.crud.CategoryCrudRepository;
 import com.store.technology.productcatalogservice.persistence.entity.Category;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,13 @@ import java.util.List;
 
 @Repository
 public class CategoryRepositoryImpl implements CategoryRepository {
+
+    private final CategoryCrudRepository categoryCrudRepository;
+
+    public CategoryRepositoryImpl(CategoryCrudRepository categoryCrudRepository) {
+        this.categoryCrudRepository = categoryCrudRepository;
+    }
+
     @Override
     public Category save(Category category) {
         return null;
