19e7357d41ed9ca8f57582576807bb783f0e9e52
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 11:16:03 -0500

    feat: Remove unused code in CategoryDTO

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/dto/CategoryDTO.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/dto/CategoryDTO.java
@@ -1,15 +1,10 @@
 package com.store.technology.productcatalogservice.domain.dto;
 
-import com.store.technology.productcatalogservice.persistence.entity.Product;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class CategoryDTO {
 
-    private String categoryId;
     private String categoryName;
     private String description;
-    private List<Product> products;
 }
