be8986d8264dc6d0b49d1604521495929d9d772f
jandres-vega <jandresvega@itsa.edu.co>
2024-09-11 19:08:25 -0500

    chore: Add API endpoint for retrieving all products

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/service/ProductService.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.store.technology.productcatalogservice.domain.service;
 
 import com.store.technology.productcatalogservice.domain.repository.ProductRepository;
+import com.store.technology.productcatalogservice.persistence.entity.Product;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -10,5 +13,9 @@ public class ProductService {
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts(){
+        return productRepository.findAll();
     }
 }
