41f0f9c9ff6cf2fdb78762d2b96ba65b6930aedc
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 17:11:56 -0500

    feat: Update ProductRepository to use request and response DTOs

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/repository/ProductRepository.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/repository/ProductRepository.java
@@ -1,11 +1,12 @@
 package com.store.technology.productcatalogservice.domain.repository;
 
-import com.store.technology.productcatalogservice.persistence.entity.Product;
+import com.store.technology.productcatalogservice.domain.dto.request.ProductRequestDTO;
+import com.store.technology.productcatalogservice.domain.dto.response.ProductResponseDTO;
 
 import java.util.List;
 
 public interface ProductRepository {
 
-    Product save(Product product);
-    List<Product> findAll();
+    ProductResponseDTO save(ProductRequestDTO product);
+    List<ProductResponseDTO> findAll();
 }
