5286bf0ddaf77cebf9055a57ddf9df72aa8e84b7
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 17:12:13 -0500

    refactor: Update ProductResponseDTO field names to match naming conventions

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/dto/response/ProductResponseDTO.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/dto/response/ProductResponseDTO.java
@@ -1,17 +1,15 @@
 package com.store.technology.productcatalogservice.domain.dto.response;
 
-import com.store.technology.productcatalogservice.persistence.entity.Image;
 import lombok.Data;
 
-import java.util.List;
+
 
 @Data
 public class ProductResponseDTO {
 
-    private String idProduct;
-    private String nameProduct;
+    private String id;
+    private String product_name;
     private String description;
-    private Double price;
-    private Integer stock;
-    private List<Image> images;
+    private double price;
+    private int stock;
 }
