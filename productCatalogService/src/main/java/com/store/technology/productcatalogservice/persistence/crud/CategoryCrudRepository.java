b9ab211b20c4c534e8483e050afbb2936db6d410
jandres-vega <jandresvega@itsa.edu.co>
2024-09-12 15:07:47 -0500

    feat: Add findByNameCategory method to CategoryCrudRepository

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/crud/CategoryCrudRepository.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/crud/CategoryCrudRepository.java
@@ -3,5 +3,8 @@ package com.store.technology.productcatalogservice.persistence.crud;
 import com.store.technology.productcatalogservice.persistence.entity.Category;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CategoryCrudRepository extends CrudRepository<Category, String> {}
+public interface CategoryCrudRepository extends CrudRepository<Category, String> {
+
+    Category findByNameCategory(String nameCategory);
+}
 
