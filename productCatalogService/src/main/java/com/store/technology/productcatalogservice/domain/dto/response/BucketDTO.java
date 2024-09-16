c7effac39dd307a8be6b0af24552f8039441b736
jandres-vega <jandresvega@itsa.edu.co>
2024-09-16 15:28:36 -0500

    feat: Add BucketDTO for managing file uploads

---
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/dto/response/BucketDTO.java
@@ -0,0 +1,16 @@
+package com.store.technology.productcatalogservice.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BucketDTO {
+
+    private String fileName;
+    private String bucket;
+}
