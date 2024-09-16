940ef95f2705aecc58f18e8b00ec86ff32f45782
jandres-vega <jandresvega@itsa.edu.co>
2024-09-16 15:30:30 -0500

    feat: Update ResourceNotFoundException message format and remove unused variables

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/exceptions/ResourceNotFoundException.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/exceptions/ResourceNotFoundException.java
@@ -1,29 +1,16 @@
 package com.store.technology.productcatalogservice.exceptions;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-@Getter
-public class ResourceNotFoundException extends RuntimeException {
-
-    private final String resourceName;
-    private String resourceFileNme;
-    private Object fieldValue;
-    private final HttpStatus httpStatus;
+public class ResourceNotFoundException extends BaseException {
 
     public ResourceNotFoundException(String resourceName, String resourceFileNme, Object fieldValue, HttpStatus httpStatus) {
-        super(resourceName);
-        this.resourceName = resourceName;
-        this.resourceFileNme = resourceFileNme;
-        this.fieldValue = fieldValue;
-        this.httpStatus = httpStatus;
+        super(resourceName, resourceFileNme, fieldValue, httpStatus);
     }
 
-    public ResourceNotFoundException(String resourceName, HttpStatus httpStatus) {
-        super(String.format("Register not founds of %s en el application ", resourceName));
-        this.resourceName = resourceName;
-        this.httpStatus = httpStatus;
+    public ResourceNotFoundException(String message, String resourceName, HttpStatus httpStatus) {
+        super(String.format("Register not found of %s in the application", resourceName), resourceName, HttpStatus.NOT_FOUND);
     }
 }
