ff726c0a2e0b75dcc2116ae3506551e1a67016b5
jandres-vega <jandresvega@itsa.edu.co>
2024-09-16 15:28:07 -0500

    feat: Add S3Client class for interacting with AWS S3 service

---
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/remote/S3Client.java
@@ -0,0 +1,20 @@
+package com.store.technology.productcatalogservice.remote;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class S3Client {
+
+    public AmazonS3 getClientAWS(String accessKey, String secretKey){
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(Regions.US_EAST_2)
+                .build();
+    }
+}
