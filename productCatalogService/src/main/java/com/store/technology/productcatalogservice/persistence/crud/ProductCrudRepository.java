8eea83f958b663122f1ca8f48aa5bc58a5623d42
jandres-vega <jandresvega@itsa.edu.co>
2024-09-16 15:29:55 -0500

    feat: Add findByNameProduct method to ProductCrudRepository

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/crud/ProductCrudRepository.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/persistence/crud/ProductCrudRepository.java
@@ -3,4 +3,6 @@ package com.store.technology.productcatalogservice.persistence.crud;
 import com.store.technology.productcatalogservice.persistence.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductCrudRepository extends CrudRepository<Product, String> { }
+public interface ProductCrudRepository extends CrudRepository<Product, String> {
+    Product findByNameProduct(String nameProduct);
+}
