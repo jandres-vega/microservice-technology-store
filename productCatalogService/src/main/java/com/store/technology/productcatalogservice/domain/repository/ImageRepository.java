d3176749f37775231655d49629a8b7c30d713b09
jandres-vega <jandresvega@itsa.edu.co>
2024-09-16 15:29:45 -0500

    feat: Add method to ImageRepository for adding image to product

--- productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/repository/ImageRepository.java
+++ productCatalogService/src/main/java/com/store/technology/productcatalogservice/domain/repository/ImageRepository.java
@@ -1,11 +1,11 @@
 package com.store.technology.productcatalogservice.domain.repository;
 
 import com.store.technology.productcatalogservice.persistence.entity.Image;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ImageRepository {
 
-    Image save(Image image);
-    List<Image> findAll();
+    Image addImageToProduct(String productId, MultipartFile file);
 }
