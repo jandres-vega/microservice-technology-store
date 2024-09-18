f27aad9224c088ad5c82d2a3bc86747646a3b694
jandres-vega <jandresvega@itsa.edu.co>
2024-09-18 12:56:20 -0500

    feat: Add UserControllers for user management service

---
+++ userManagementService/src/main/java/com/store/technology/usermanagementservice/web/controllers/UserControllers.java
@@ -0,0 +1,28 @@
+package com.store.technology.usermanagementservice.web.controllers;
+
+import com.store.technology.usermanagementservice.domain.dto.request.UserRequestDTO;
+import com.store.technology.usermanagementservice.domain.dto.response.UserResponseDTO;
+import com.store.technology.usermanagementservice.domain.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserControllers {
+
+    private final UserService userService;
+
+    public UserControllers(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO userRequestDTO) {
+        System.out.println("UserRequestDTO: " + userRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userRequestDTO));
+    }
+}
