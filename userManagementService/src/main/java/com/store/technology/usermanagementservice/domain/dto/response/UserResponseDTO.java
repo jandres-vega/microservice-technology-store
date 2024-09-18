0079c7edbc145830883abab685b6f6015c1850fe
jandres-vega <jandresvega@itsa.edu.co>
2024-09-18 12:55:47 -0500

    feat: Add UserResponseDTO for user management service

---
+++ userManagementService/src/main/java/com/store/technology/usermanagementservice/domain/dto/response/UserResponseDTO.java
@@ -0,0 +1,11 @@
+package com.store.technology.usermanagementservice.domain.dto.response;
+
+import lombok.Data;
+
+@Data
+public class UserResponseDTO {
+
+    private String id;
+    private String user_name;
+    private String email;
+}
