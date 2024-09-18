10bd689aab93859bf254c1f0cd6206dd1ea4e986
jandres-vega <jandresvega@itsa.edu.co>
2024-09-18 11:06:21 -0500

    feat: Add User entity for user management service

---
+++ userManagementService/src/main/java/com/store/technology/usermanagementservice/persistence/entity/User.java
@@ -0,0 +1,29 @@
+package com.store.technology.usermanagementservice.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id_user")
+    private String idUser;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String phone;
+    private String email;
+    private String password;
+    private Boolean active;
+    @OneToMany(mappedBy = "user")
+    private List<Address> addresses;
+}
