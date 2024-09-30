ad098deaf20b7945786f29166ad4514c7194438c
jandres-vega <jandresvega@itsa.edu.co>
2024-09-30 09:19:57 -0500

    Add ConfigCors class for CORS configuration

---
+++ authentication/src/main/java/com/store/technology/authentication/web/config/ConfigCors.java
@@ -0,0 +1,24 @@
+package com.store.technology.authentication.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class ConfigCors {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "DELETE", "UPDATE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
