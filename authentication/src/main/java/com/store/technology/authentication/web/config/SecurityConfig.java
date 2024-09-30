b802d187e3655ae9aecdbfa67368dde5e5cfd659
jandres-vega <jandresvega@itsa.edu.co>
2024-09-30 09:19:30 -0500

    Refactorizar configuración de la aplicación de autenticación y añadir clase SecurityConfig

---
+++ authentication/src/main/java/com/store/technology/authentication/web/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.store.technology.authentication.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(){
+        return null;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManage(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
