aa6ff57da868ddb1ac15f1b6de5e7c5443c3f224
jandres-vega <jandresvega@itsa.edu.co>
2024-10-03 11:00:01 -0500

    Add spring-cloud-starter-openfeign dependency for API communication

---
+++ userManagementService/src/main/java/com/store/technology/usermanagementservice/domain/client/AuthServiceClient.java
@@ -0,0 +1,8 @@
+package com.store.technology.usermanagementservice.domain.client;
+import com.store.technology.usermanagementservice.domain.dto.response.AuthCredentialDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "authentication", url = "http://localhost:5000/api/authentication")
+public interface AuthServiceClient {
+    AuthCredentialDTO registerCredential();
+}
---
+++ userManagementService/src/main/java/com/store/technology/usermanagementservice/domain/dto/response/AuthCredentialDTO.java
@@ -0,0 +1,10 @@
+package com.store.technology.usermanagementservice.domain.dto.response;
+
+import lombok.Data;
+
+@Data
+public class AuthCredentialDTO {
+
+    private String idCredential;
+    private String email;
+}
